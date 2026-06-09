a08e67ef05a02507cce83501c98731ce757f5ab4
gichie534 <richardbatyrov@gmail.com>
2026-06-09 20:00:41 +0400

    add deletion_protection var to folder module

--- modules/gcp/folder/main.tf
+++ modules/gcp/folder/main.tf
@@ -1,4 +1,5 @@
 resource "google_folder" "this" {
-  display_name = var.display_name
-  parent       = var.parent
+  display_name        = var.display_name
+  parent              = var.parent
+  deletion_protection = var.deletion_protection
 }
--- modules/gcp/folder/variables.tf
+++ modules/gcp/folder/variables.tf
@@ -7,3 +7,9 @@ variable "parent" {
   description = "Parent resource: organizations/<org_id> or folders/<folder_id>"
   type        = string
 }
+
+variable "deletion_protection" {
+  description = "Whether to prevent the folder from being deleted while this value is true"
+  type        = bool
+  default     = true
+}
